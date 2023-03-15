dab98e6b8660721a43c7d57cebdd1557c3d17435
OH-Neuri <87141845+OH-Neuri@users.noreply.github.com>
2023-03-15 23:44:06 +0900

    [DESIGN] #S08P22A707-192 TopNav 반응형 디자인 작업

--- Front/sulnaeeum/src/components/TopNav.tsx
+++ Front/sulnaeeum/src/components/TopNav.tsx
@@ -2,17 +2,34 @@ import Link from "next/link";
 
 export default function App() {
   return (
-    <div>
-      <Link href="/">로고 부분 </Link>
-      <div>
-        <Link href="/">전통주</Link>
-        <Link href="/">지도</Link>
-        <Link href="/">랭킹</Link>
-        <Link href="/">추천</Link>
-        <Link href="/">전통주 유형검사</Link>
+    <>
+      {/** 기본 배경: 흰색, hover: bg-[#DAD2C7] */}
+      <div className={"h-[50px] bg-blue-400 grid grid-cols-6 "}>
+        <div className={"col-start-1 col-end-2"}>
+          <Link href="/">로고 부분 </Link>
+        </div>
+        <div className={"col-start-2 col-end-6"}>
+          <Link className={""} href="/list">
+            전통주
+          </Link>
+          <Link className={""} href="/map">
+            지도
+          </Link>
+          <Link className={""} href="/rank">
+            랭킹
+          </Link>
+          <Link className={""} href="/recommend">
+            추천
+          </Link>
+          <Link className={""} href="/jubti">
+            전통주 유형검사
+          </Link>
+        </div>
+        <div className={"col-start-7 col-end-8"}>
+          <button>검색</button>
+          <Link href="/user">로그인</Link>
+        </div>
       </div>
-      <div></div>
-      <div></div>
-    </div>
+    </>
   );
 }
--- Front/sulnaeeum/src/pages/_document.tsx
+++ Front/sulnaeeum/src/pages/_document.tsx
@@ -1,14 +1,15 @@
-import TopNav from '@/components/TopNav'
-import { Html, Head, Main, NextScript } from 'next/document'
+import TopNav from "@/components/TopNav";
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <TopNav />
+      <Head />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
