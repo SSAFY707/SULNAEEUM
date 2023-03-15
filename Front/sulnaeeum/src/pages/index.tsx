653fdff64ad6c1e35837e6aa2f192e9286490f84
김나현 <skgusdl7403@naver.com>
2023-03-15 15:44:21 +0900

    [FEAT] #S08P22A707-125 프로젝트 생성

---
+++ Front/sulnaeeum/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>SULNAEEUM | 술내음</title>
+      </Head>
+        
+
+    </>
+  )
+}
