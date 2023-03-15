d56c4bb86a51e610ab2e6d4ba5e4e664cd693495
이성훈 <leesh940312@gmail.com>
2023-03-15 16:34:01 +0900

    [FEAT] #S08P22A707-144  프로젝트 생성

--- Front/sulnaeeum/src/pages/_document.tsx
+++ Front/sulnaeeum/src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head><title>SULNAEEUM | 술내음</title></Head>
+      
+
       <body>
         <Main />
         <NextScript />
