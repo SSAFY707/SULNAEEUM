4f6fb6565fd244cc3083ce01bb8f0d3961962270
이성훈 <leesh940312@gmail.com>
2023-03-17 18:06:21 +0900

    [FEAT] #S20123123

--- Front/sulnaeeum/src/pages/index.tsx
+++ Front/sulnaeeum/src/pages/index.tsx
@@ -1,11 +1,7 @@
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>SULNAEEUM | 술내음</title>
-      </Head>
-        
-
+    
     </>
   )
 }
--- Front/sulnaeeum/src/pages/map/index.tsx
+++ Front/sulnaeeum/src/pages/map/index.tsx
@@ -7,10 +7,27 @@ export default function index() {
     <>
       <div className={style.container}>
         <h1>지도 페이지</h1>
-        <div className={style.lay && style.mapContainer}>
-          <img src="images/map/전국지도.png" alt="지도 이미지" width="500"></img>
+        <div className={`${style.mapContainer} ${style.layout}`}>
+          <img src="images/map/전국지도.png" alt="지도 이미지" width="300"></img>
+          <div className={`${style.map1}`}>
+            <img src="images/map/경기도.png" alt="지도 이미지" width="158"                                        ></img>
+          </div>
+          <div className={`${style.map2}`}>
+            <img src="images/map/강원도.png" alt="지도 이미지" width="178"                                        ></img>
+          </div>
+          <div className={`${style.map3}`}>
+            <img src="images/map/충청도.png" alt="지도 이미지" width="190"                                        ></img>
+          </div>
+          <div className={`${style.map4}`}>
+            <img src="images/map/전라도.png" alt="지도 이미지" width="174"                                        ></img>
+          </div>
+          <div className={`${style.map5}`}>
+            <img src="images/map/경상도.png" alt="지도 이미지" width="154"                                        ></img>
+          </div>
+          <div className={`${style.map6}`}>
+            <img src="images/map/제주도.png" alt="지도 이미지" width="154"                                        ></img>
+          </div>
         </div>
-
       </div>
     </>
   )
