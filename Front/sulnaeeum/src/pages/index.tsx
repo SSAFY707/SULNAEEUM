1808690613403e3b091e5888bee9a11388c3f937
OH-Neuri <87141845+OH-Neuri@users.noreply.github.com>
2023-03-20 22:07:43 +0900

    [REFACT] #S08P22A707-346 메인 페이지 폴더 구조 정리

--- Front/sulnaeeum/src/pages/index.tsx
+++ Front/sulnaeeum/src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import useScroll from "../assets/useScroll";
 import styles from "./styles.module.css";
-import { useRef, useEffect, useState} from 'react';
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const scroll = useScroll();
@@ -15,8 +15,7 @@ export default function Home() {
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
-  const imageX = (scrollY / 5)>185?185:(scrollY / 5);
-
+  const imageX = scrollY / 5 > 185 ? 185 : scrollY / 5;
 
   return (
     <>
@@ -29,20 +28,37 @@ export default function Home() {
           <p>translateY :{translateY}</p>
           <p>image : {imageX}</p>
         </div>
-      
+
         {/*흰 텍스트1 */}
-        <div className={ `${styles.scrolled} w-[4500px] ${isScrolled?"absolute z-30":"fixed"}  z-20 w-[3000px]`} style={{ top:`${isScrolled?"1250px":"10px"}`,  left: `${-imageX*14}px`}}> 
+        <div
+          className={`${styles.scrolled} w-[4500px] ${
+            isScrolled ? "absolute z-30" : "fixed"
+          }  z-20 w-[3000px]`}
+          style={{
+            top: `${isScrolled ? "1250px" : "10px"}`,
+            left: `${-imageX * 14}px`,
+          }}
+        >
           <Image src="/white_txt_1.png" alt="" width={6000} height={920} />
         </div>
 
         {/*전통주 배경화면 */}
-        <div  className={` w-[4000px] h-[300px] ${isScrolled ? " absolute z-10 top-[1250px]" : "fixed"}`}>
-          <Image className="z-20" src="/main_1.jpg" alt="" width={1920} height={0} />
-            <div className=" absolute z-40 top-[650px]">
-              <Image src="/white_txt_2.png" alt="" width={1920} height={0} />
-            </div>
+        <div
+          className={` w-[4000px] h-[300px] ${
+            isScrolled ? " absolute z-10 top-[1250px]" : "fixed"
+          }`}
+        >
+          <Image
+            className="z-20"
+            src="/main_1.jpg"
+            alt=""
+            width={1920}
+            height={0}
+          />
+          <div className=" absolute z-40 top-[650px]">
+            <Image src="/white_txt_2.png" alt="" width={1920} height={0} />
+          </div>
         </div>
-        <div className="mt-[280px] bg-sky-400 w-[1920px] h-[1008px]"></div>
       </div>
     </>
   );
