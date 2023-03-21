af027615a9621edcd2d1ad31cc2928aaaabb8090
김나현 <skgusdl7403@naver.com>
2023-03-21 10:04:03 +0900

    [FIX] jubti merge 완

--- Front/sulnaeeum/src/pages/_app.tsx
+++ Front/sulnaeeum/src/pages/_app.tsx
@@ -1,9 +1,13 @@
-
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import NavBar from "../components/NavBar";
 
+declare global {
+  interface Window {
+    Kakao: any;
+  }
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
