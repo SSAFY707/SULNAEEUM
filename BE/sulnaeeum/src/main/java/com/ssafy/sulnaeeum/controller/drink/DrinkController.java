d8051498583c5bc8028389643199828007c908da
김설희 <2750seolhee@naver.com>
2023-03-21 14:57:34 +0900

    [FEAT] 전통주 페이지 회원, 비회원 구분하여 찜 여부 조회 기능 구현

--- BE/sulnaeeum/src/main/java/com/ssafy/sulnaeeum/controller/drink/DrinkController.java
+++ BE/sulnaeeum/src/main/java/com/ssafy/sulnaeeum/controller/drink/DrinkController.java
@@ -27,15 +27,19 @@ public class DrinkController {
     @Autowired
     LikeDrinkService likeDrinkService;
 
+    // =================================================================================================================
+    // -----------------------------------------------   [ 회원 ]   -----------------------------------------------------
+    // =================================================================================================================
+
     /***
      * [ 모든 전통주 조회 ]
      * - 조회한 내용을 필요한 데이터만 선별하여 반환 (id, 이름, 이미지, 양, 도수, 주종, 찜 여부)
      * - 이름, 인기, 도수(높은 순, 낮은 순) 기준 정렬 (인기, 도수의 경우 같을 경우에는 이름으로 정렬)
      * - 카테고리 분류
      ***/
-    @Operation(summary = "모든 전통주 조회", description = "전체 전통주를 데이터 가공하여 필요한 정보만 정렬 및 분류하여 제공")
+    @Operation(summary = "모든 전통주 조회 - 회원", description = "전체 전통주를 데이터 가공하여 필요한 정보만 정렬 및 분류하여 제공")
     @GetMapping("/{drinkTypeName}")
-    public ResponseEntity<List<DrinkInfoDto>> getAllDrink(@PathVariable String drinkTypeName, @RequestParam String sortType) {
+    public ResponseEntity<List<DrinkInfoDto>> getAllDrinkForUser(@PathVariable String drinkTypeName, @RequestParam String sortType) {
         String kakaoId = SecurityContextHolder.getContext().getAuthentication().getName();
         return new ResponseEntity<>(drinkService.getAllDrink(drinkTypeName, kakaoId, sortType), HttpStatus.OK);
     }
@@ -44,22 +48,28 @@ public class DrinkController {
      * [ 전통주 찜 ]
      * - 기존에 찜 되어있지 않을 경우, DB에 찜 한 내용 저장 (찜 한 회원, 전통주)
      * - 기존에 찜 되어있을 경우, DB에서 기존에 찜 했던 내용 삭제
-     */
-    @Operation(summary = "전통주 찜", description = "찜하거나 찜을 취소")
+     ***/
+    @Operation(summary = "전통주 찜 - 회원", description = "찜하거나 찜을 취소")
     @PostMapping("/like/{drinkId}")
-    public ResponseEntity<String> switchLikeDrink(@PathVariable Long drinkId) {
+    public ResponseEntity<String> switchLikeDrinkForUser(@PathVariable Long drinkId) {
         String kakaoId = SecurityContextHolder.getContext().getAuthentication().getName();
         return new ResponseEntity<>(likeDrinkService.switchLikeDrink(drinkId, kakaoId), HttpStatus.OK);
     }
 
+    // =================================================================================================================
+    // ----------------------------------------------   [ 비회원 ]   ----------------------------------------------------
+    // =================================================================================================================
+
     /***
-     * [ 전통주 검색 ]
-     * - 키워드로 전통주 검색
-     * - 검색 결과로 필요한 데이터만 선별하여 반환
-     */
-    @Operation(summary = "전통주 검색", description = "전통주를 키워드로 검색")
-    @PutMapping
-    public ResponseEntity<List<DrinkSearchDto>> searchDrink(@RequestParam String search) {
-        return new ResponseEntity<>(drinkService.drinkSearch(search), HttpStatus.OK);
+     * [ 모든 전통주 조회 ]
+     * - 조회한 내용을 필요한 데이터만 선별하여 반환 (id, 이름, 이미지, 양, 도수, 주종, 찜 여부)
+     * - 이름, 인기, 도수(높은 순, 낮은 순) 기준 정렬 (인기, 도수의 경우 같을 경우에는 이름으로 정렬)
+     * - 카테고리 분류
+     * - 찜 여부는 모두 false 반환
+     ***/
+    @Operation(summary = "모든 전통주 조회 - 비회원", description = "전체 전통주를 데이터 가공하여 필요한 정보만 정렬 및 분류하여 제공")
+    @GetMapping("/n/{drinkTypeName}")
+    public ResponseEntity<List<DrinkInfoDto>> getAllDrink(@PathVariable String drinkTypeName, @RequestParam String sortType) {
+        return new ResponseEntity<>(drinkService.getAllDrink(drinkTypeName, null, sortType), HttpStatus.OK);
     }
 }
