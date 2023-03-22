449506faa07a313e8f737f5fc9e076325f403297
김나현 <skgusdl7403@naver.com>
2023-03-22 17:48:19 +0900

    [FEAT] 클리어 버튼 컴포넌트 생성

---
+++ Front/sulnaeeum/src/components/list/clearBtn.tsx
@@ -0,0 +1,17 @@
+export const ClearBtn = () => {
+    return (
+        <div className={"cursor-pointer flex justify-center items-center text-white bg-[#78C3DC] rounded-full hover:bg-[#60A5BC] font-preEL text-[20px] w-[80px] h-[40px]"}>clear</div>
+    )
+}
+
+export const ClearTrue = () => {
+    return (
+        <div className={"flex justify-center items-center text-[#78C3DC] border border-[#78C3DC] rounded-full font-preEL text-[20px] w-[80px] h-[40px]"}>clear</div>
+    )
+}
+
+export const ClearFalse = () => {
+    return (
+        <div className={"flex justify-center items-center text-[#D3D3D3] border border-[#D3D3D3] rounded-full font-preEL text-[20px] w-[80px] h-[40px]"}>clear</div>
+    )
+}
--- Front/sulnaeeum/src/pages/list/[drink_id].tsx
+++ Front/sulnaeeum/src/pages/list/[drink_id].tsx
@@ -1,10 +1,15 @@
+import { ClearBtn, ClearFalse, ClearTrue } from '@/components/list/clearBtn'
 import React from 'react'
 
 export default function Detail() {
   return (
     <>
       <div>
-        
+        <div className={'flex justify-center items-center w-[300px] h-[300px]'}>
+          <ClearBtn />
+          <ClearTrue />
+          <ClearFalse />
+          </div>
       </div>
     </>
   )
