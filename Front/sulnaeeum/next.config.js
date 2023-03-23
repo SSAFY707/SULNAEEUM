12033b935d4fa0411f1446fdbbc8da4ec0146f53
이성훈 <leesh940312@gmail.com>
2023-03-23 17:40:38 +0900

    [FEAT] #S08P22A707-318 지도페이지 merge

--- Front/sulnaeeum/next.config.js
+++ Front/sulnaeeum/next.config.js
@@ -2,45 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
 
-  async redirects(){ 
-    return [
-      {
-        source: '/list',
-        destination: '/test',
-        permanent: true,
-      },
-      {
-        source: '/map',
-        destination: '/test',
-        permanent: true,
-      },
-      {
-        source: '/random',
-        destination: '/test',
-        permanent: true,
-      },
-      {
-        source: '/rank',
-        destination: '/test',
-        permanent: true,
-      },
-      {
-        source: '/recommend',
-        destination: '/test',
-        permanent: true,
-      },
-      {
-        source: '/user',
-        destination: '/test',
-        permanent: true,
-      },
-      {
-        source: '/',
-        destination: '/test',
-        permanent: true,
-      },
-    ];
-  }
+
 }
 
 module.exports = nextConfig
