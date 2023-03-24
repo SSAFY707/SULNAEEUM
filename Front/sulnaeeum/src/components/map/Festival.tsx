b5e26475fa1a0bfd0ddc37f8e09f93d145c5b31e
이성훈 <leesh940312@gmail.com>
2023-03-24 16:08:29 +0900

    [FEAT] #S08P22A707-323 지도페이지 마무리 작업 및 연동

--- Front/sulnaeeum/src/components/map/Festival.tsx
+++ Front/sulnaeeum/src/components/map/Festival.tsx
@@ -1,103 +1,49 @@
 import React from 'react'
-import style from './map.module.css'
+import style from '@/pages/map/map.module.css'
 import { FaBookmark } from 'react-icons/fa'
 import { useState } from 'react';
 export default function Festival(props: any) {
 
-  const { tab } = props
+  const { fest, mode, tab } = props
 
   return (<>
-    <div className='w-[584px] scroll h-[680px] overflow-y-scroll'>
+    <div className='w-[584px] scroll h-[680px] overflow-hidden'>
 
+      <div className='w-full h-[680px] '>
+        <div className='flex justify-start w-full h-[440px] mt-[26px]'>
+          <img src={`/images/map/fest/fest${mode}.png`} alt='축제 포스터' className='object-cover'></img>
+          <div className='flex-col items-center w-full h-full'>
+            <h1 className='mt-[110px] text-center text-[32px]'>{fest[mode].fest_name}</h1>
+            <p className='text-center'>({fest[mode].start_date.substr(2, 8).replace(/-/g, '.')} ~ {fest[mode].end_date.substr(2, 8).replace(/-/g, '.')})</p>
+            <div className='h-[50px] mt-[20px] text-center'>{fest[mode].location}</div>
+            <a href={fest[mode].url} className='rounded-[2px] mt-[20px] ml-[36px] w-[210px] h-[44px] flex justify-center items-center text-white bg-[#5CABB7] hover:bg-[#3C97A5]'>
+              <p>축제 바로가기</p>
+            </a>
 
-      <div className={`${style.item_box}`}>
-        <div className={`${style.item_up}`}>
-          <img src='/images/map/items/03.png' alt='이미지' className='w-[140px] h-[140px] object-cover'></img>
-          <div className={`${style.text_area}`}>
-            <div className='flex w-full h-[50px] justify-between items-center'>
-              <h3 className='text-[26px] font-bold'>안산시 우리술 축제</h3>
-              <FaBookmark className='text-[#ADA7A3] text-[22px]' />
-            </div>
-            <div className='w-full h-[110px]'>
-              <div className='flex w-full h-[110px]'>
-                <div className={`${style.text_left}`}>
-                  <p>주소</p>
-                  <p>주종</p>
-                  <p>연락처</p>
-                </div>
-                <div className={`${style.text_right}`}>
-                  <p>동대문구 장안동 444-2</p>
-                  <p>탁주, 증류주, 과실주</p>
-                  <p>02-256-1423</p>
-                </div>
-              </div>
-            </div>
           </div>
         </div>
-        <div className={`${style.item_down}`}>
-          <img src='/images/line.png'></img>
+
+        <div className='w-full h-full'>
+          <div className='mt-[32px] text-[20px]'>{fest[mode].contents.substr(0, 290)}</div>
         </div>
       </div>
 
-      {/* 반복문으로 만들 부분 */}
-      <div className={`${style.item_box}`}>
-        <div className={`${style.item_up}`}>
-          <img src='/images/map/items/01.png' alt='이미지' className='w-[140px] h-[140px] object-cover'></img>
-          <div className={`${style.text_area}`}>
-            <div className='flex w-full h-[50px] justify-between items-center'>
-              <h3 className='text-[26px] font-bold'>나현이네 축제</h3>
-              <FaBookmark className='text-[#ADA7A3] text-[22px]' />
-            </div>
-            <div className='w-full h-[110px]'>
-              <div className='flex w-full h-[110px]'>
-                <div className={`${style.text_left}`}>
-                  <p>주소</p>
-                  <p>주종</p>
-                  <p>연락처</p>
-                </div>
-                <div className={`${style.text_right}`}>
-                  <p>동대문구 장안동 444-2</p>
-                  <p>탁주, 증류주, 과실주</p>
-                  <p>02-256-1423</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className={`${style.item_down}`}>
-          <img src='/images/line.png'></img>
-        </div>
-      </div>
-      <div className={`${style.item_box}`}>
-        <div className={`${style.item_up}`}>
-          <img src='/images/map/items/04.png' alt='이미지' className='w-[140px] h-[140px] object-cover'></img>
-          <div className={`${style.text_area}`}>
-            <div className='flex w-full h-[50px] justify-between items-center'>
-              <h3 className='text-[26px] font-bold'>하늘이네 축제 한마당</h3>
-              <FaBookmark className='text-[#ADA7A3] text-[22px]' />
-            </div>
-            <div className='w-full h-[110px]'>
-              <div className='flex w-full h-[110px]'>
-                <div className={`${style.text_left}`}>
-                  <p>주소</p>
-                  <p>주종</p>
-                  <p>연락처</p>
-                </div>
-                <div className={`${style.text_right}`}>
-                  <p>동대문구 장안동 444-2</p>
-                  <p>탁주, 증류주, 과실주</p>
-                  <p>02-256-1423</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className={`${style.item_down}`}>
-          <img src='/images/line.png'></img>
-        </div>
-      </div>
 
-      {/* 반복문으로 만들 부분 */}
+
+      <div>===========================</div>
+      <div>{mode}</div>
+      <div>{tab}</div>
+      <div>{fest[mode].fest_name}</div>
+      <div>{fest[mode].start_date}</div>
+      <div>{fest[mode].end_date}</div>
+      <div>{fest[mode].location}</div>
+      <div>{fest[mode].url}</div>
+      <div>{fest[mode].contents}</div>
+
+
+
+
+
 
 
 
