da24d54d79f461f2b9208de798f3270f2329810f
김설희 <2750seolhee@naver.com>
2023-03-27 20:02:53 +0900

    [REFACT] 리팩토링

--- BE/sulnaeeum/src/main/java/com/ssafy/sulnaeeum/model/drink/entity/MyDrink.java
+++ BE/sulnaeeum/src/main/java/com/ssafy/sulnaeeum/model/drink/entity/MyDrink.java
@@ -1,6 +1,5 @@
 package com.ssafy.sulnaeeum.model.drink.entity;
 
-import com.ssafy.sulnaeeum.model.drink.entity.Drink;
 import com.ssafy.sulnaeeum.model.user.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
