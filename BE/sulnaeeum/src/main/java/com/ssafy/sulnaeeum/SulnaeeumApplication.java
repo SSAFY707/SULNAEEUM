a43f7e1881e4827049e9f97bd9701c40f94d7b87
김원규 <re9946@naver.com>
2023-03-29 14:10:29 +0900

    [FEAT] 랭킹 업데이트 기능 추가

--- BE/sulnaeeum/src/main/java/com/ssafy/sulnaeeum/SulnaeeumApplication.java
+++ BE/sulnaeeum/src/main/java/com/ssafy/sulnaeeum/SulnaeeumApplication.java
@@ -2,8 +2,10 @@ package com.ssafy.sulnaeeum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SulnaeeumApplication {
 
 	public static void main(String[] args) {
