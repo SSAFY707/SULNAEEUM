d20ec7b9aa16bba67768a7575492d33f5f5b47b7
김나현 <skgusdl7403@naver.com>
2023-03-29 23:07:34 +0900

    [DESIGN] 랭킹 페이지 레이아웃

--- Front/sulnaeeum/src/pages/rank/index.tsx
+++ Front/sulnaeeum/src/pages/rank/index.tsx
@@ -1,8 +1,15 @@
 export default function index() {
   return (
     <>
-      <div className="absoulte w-[400px] h-[400px] top-[70px] bg-yellow-300">
-        랭킹 페이지 입니다.
+      <div className="flex flex-col items-center w-screen h-screen bg-zinc-200">
+        <div className={'flex justify-between w-7/12 mt-32'}>
+          <div className={''}>
+            <div className={'text-[26px] font-preM'}>전통주 랭킹</div>
+            <div>Traditional Liquor Ranking</div>
+          </div>
+          <div>어떻게 랭킹 나눌까ㅏㅏㅏㅏ</div>
+        </div>
+        <div></div>
       </div>
     </>
   );
