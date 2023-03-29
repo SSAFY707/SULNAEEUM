ba4e1a55832462bdfa459cfe45c87bac509b58a5
오하늘 <moho009804@gmail.com>
2023-03-29 14:56:26 +0900

    [FEAT] #S08P22A707-370  랜덤 오늘의 페이지 레이아웃 및 디자인 작업

--- Front/sulnaeeum/src/components/recommend/todayModal.tsx
+++ Front/sulnaeeum/src/components/recommend/todayModal.tsx
@@ -15,33 +15,63 @@ import Image from "next/image";
 export default function todayModal(props: {
   todayType: string;
   image: string;
+  todayText: string[];
 }) {
-  const { image, todayType } = props;
+  
+  const { image, todayType,todayText } = props;
   const { isOpen, onOpen, onClose } = useDisclosure();
+
+
   return (
     <div>
+      <div className="w-[360px] h-[360px] flex justify-center items-center ">
       <div
-        className="w-[350px] h-[350px] bg-red-300 rounded-lg"
+        className="w-[350px] h-[350px] bg-white rounded-lg hover:w-[360px] hover:h-[360px] hover:cursor-pointer shadow-lg"
         onClick={onOpen}
-      >
-        <div className="absolute mt-[50px]">
-          <div className="ml-[50px]">
-            <Image src={image} alt="" width={70} height={70}></Image>
+        >
+          <div className="absolute mt-[50px]">
+            <div className="ml-[50px]">
+              <Image src={image} alt="" width={85} height={85}></Image>
+            </div>
+            <div className=" ml-[50px] mt-[15px] text-[30px] font-preSB text-[#414141]">
+              {todayType}
+            </div>
+            <div className="w-[250px] h-[100px] ml-[50px] mt-[10px] ">
+              {todayText.map((v, i) => { 
+                return (
+                  <span className="leading-relaxed font-preR text-[16px] text-[#7F7F7F]" key={i}>{v}    </span>
+                )
+              })}
+            </div>
           </div>
-          <div className=" ml-[50px] mt-[10px] text-[25px] font-preSB">
-            {todayType}
-          </div>
-        </div>
       </div>
-
-      <Modal onClose={onClose} isOpen={isOpen} isCentered>
+    </div>
+      <Modal onClose={onClose} size="lg" isOpen={isOpen} isCentered>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
+          <ModalHeader >{todayType}</ModalHeader>
           <ModalCloseButton />
-          <ModalBody></ModalBody>
+          <ModalBody>
+            <div className="w-[460px] h-[300px]  flex-col flex  bg-red-200 items-center">
+              <div className=" w-[200px] h-[50px] bg-yellow-200 flex justify-center">
+              <div className=" mr-[15px]">
+                <Image src="/images/recommend/congratulations.png" alt="" width={50} height={50} ></Image>
+              </div>
+              <div className="w-[80px] h-[200px] text-[#494949] flex justify-center text-[35px] font-preEB">
+                결과
+              </div>
+              <div className="ml-[15px] ">
+                <Image src="/images/recommend/congratulations2.png" alt="" width={50} height={50} ></Image>
+              </div>
+              </div>
+              <div className="w-[400px] h-[400px] bg-pink-300 flex justify-center">
+                여기다가 데이터를 받아서 넣으슈
+              </div>
+            </div>
+          </ModalBody>
           <ModalFooter>
-            <Button onClick={onClose}>Close</Button>
+            <Button >다시 돌리기</Button>
+            <Button colorScheme='orange' className="ml-[15px]" onClick={onClose}>확인</Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
--- Front/sulnaeeum/src/pages/recommend/today.tsx
+++ Front/sulnaeeum/src/pages/recommend/today.tsx
@@ -3,30 +3,39 @@ import TodayModal from "@/components/recommend/todayModal";
 export default function today() {
   const todayType = ["오늘의 건배사", "오늘의 전통주", "오늘의 안주"];
   const image = [
-    "/images/recommend/건배사.png",
+    "/images/recommend/건배사2.png",
     "/images/recommend/전통주.png",
     "/images/recommend/안주.png",
   ];
+  
+  const todayText = [
+    ["#청바지, 청춘은 바로 지금!", "#진달래, 진하고 달콤한 내일을 위하여!", "#통통통, 의사소통! 운수대통! 만사형통!"],
+    ["#남산에 레드와인", "#배꽃 필 무렵", "#서울의 밤", "#얼떨결에", "#제주 낭만", "#호산춘", "#아이싱 자몽","#볼빨간 미자"],
+    ["#삼겹살","#족발","#두부김치","#감자탕","#어묵탕","#번데기탕","#김치찌개",'#회',"#후라이드","#피자","#골뱅이무침"]
+  ]
+
 
   return (
     <div className="flex justify-center ">
       <div className="absolute top-[120px] text-[18px] font-preL text-[#494949]">
-        today's series
+        random recommendation service
       </div>
       <div className="absolute top-[145px] text-[55px] font-preEB text-[#655443]">
-        오늘의 시리즈{" "}
+        랜덤 추천 서비스{" "}
       </div>
       <div className="absolute top-[230px] text-[#7F7F7F] text-[16px]">
         {" "}
-        매일 랜덤으로 지정되는 오늘의 시리즈를 통해 건배사, 전통주, 안주를 추천
-        받아보세요.{" "}
+
+        랜덤으로 지정해주는 추천 서비스를 통해 건배사, 전통주, 안주 고르는 시간을 줄여보세요!{" "}
       </div>
-      <div className="absolute top-[400px] w-[1200px] h-[350px] bg-indigo-200 flex justify-between">
+      <div className="absolute top-[320px] w-[2000px] h-[480px] items-center bg-[#E8D9CD] rounded-sm flex justify-evenly">
+        <div className="absolute  w-[1500px] h-[480px] items-center bg-[#E8D9CD] rounded-sm flex justify-evenly">
         {todayType.map((v, i) => {
           return (
-            <TodayModal todayType={v} image={image[i]} key={i}></TodayModal>
+            <TodayModal todayType={v} todayText={todayText[i]} image={image[i]} key={i}></TodayModal>
           );
         })}
+          </div>
       </div>
     </div>
   );
