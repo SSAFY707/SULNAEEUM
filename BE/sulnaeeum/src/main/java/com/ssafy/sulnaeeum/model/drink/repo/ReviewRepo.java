9cecc49fc747ea8f2233a2ad89dd90111535cceb
김설희 <2750seolhee@naver.com>
2023-03-29 13:03:01 +0900

    [FEAT] 리뷰 작성, 수정, 삭제 시 랭킹 갱신 기능 구현

--- BE/sulnaeeum/src/main/java/com/ssafy/sulnaeeum/model/drink/repo/ReviewRepo.java
+++ BE/sulnaeeum/src/main/java/com/ssafy/sulnaeeum/model/drink/repo/ReviewRepo.java
@@ -18,4 +18,8 @@ public interface ReviewRepo extends JpaRepository<Review, Long> {
     // 해당 전통주의 모든 리뷰 조회
     @Query(value = "select * from review where drink_id = ?1", nativeQuery = true)
     List<Review> findAllByDrinkId(Long drinkId);
+
+    // 내 리뷰 개수 조회
+    @Query(value = "select * from review where user_id = ?1", nativeQuery = true)
+    List<Review> findByUserId(Long userId);
 }
