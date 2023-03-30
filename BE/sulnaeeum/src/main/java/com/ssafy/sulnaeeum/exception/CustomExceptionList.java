3e2958e8d9e9e6b8abd26c2e281bbb58977cd48d
김설희 <2750seolhee@naver.com>
2023-03-30 11:02:27 +0900

    [UPDATE] 전통주 전체 조회 정렬 및 붕류 기능 성능 개선

--- BE/sulnaeeum/src/main/java/com/ssafy/sulnaeeum/exception/CustomExceptionList.java
+++ BE/sulnaeeum/src/main/java/com/ssafy/sulnaeeum/exception/CustomExceptionList.java
@@ -14,7 +14,9 @@ public enum CustomExceptionList {
     TOKEN_VALID_FAILED(HttpStatus.UNAUTHORIZED, "E004", "유효하지 않은 토큰입니다."),
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "E005", "존재하지 않는 회원입니다."),
     ROW_NOT_FOUND(HttpStatus.NOT_FOUND, "E006", "존재하지 않는 행입니다."),
-    PERMISSION_DENIED(HttpStatus.FORBIDDEN, "E007", "잘못된 권한입니다.");
+    PERMISSION_DENIED(HttpStatus.FORBIDDEN, "E007", "잘못된 권한입니다."),
+    SORT_TYPE_NOT_FOUND(HttpStatus.NOT_FOUND, "E008", "올바른 정렬 요청이 아닙니다."),
+    CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "E009", "올바른 카테고리 요청이 아닙니다.");
 
     private final HttpStatus status;
     private final String code;
