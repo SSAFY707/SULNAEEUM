2624c9b5038ae8940507c6e83daea9d9ba2b87c3
김설희 <2750seolhee@naver.com>
2023-03-30 16:11:24 +0900

    [FIX] 깃 충돌 해결

--- BE/sulnaeeum/src/main/java/com/ssafy/sulnaeeum/model/drink/repo/TasteRepo.java
+++ BE/sulnaeeum/src/main/java/com/ssafy/sulnaeeum/model/drink/repo/TasteRepo.java
@@ -3,9 +3,14 @@ package com.ssafy.sulnaeeum.model.drink.repo;
 import com.ssafy.sulnaeeum.model.drink.entity.Drink;
 import com.ssafy.sulnaeeum.model.drink.entity.Taste;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface TasteRepo extends JpaRepository<Taste, Long> {
+
+    @Query(value = "select * from taste where drink_id = ?1", nativeQuery = true)
+    List<Taste> findTaste(Long drinkId);
+
     List<Taste> findByDrink(Drink drink);
 }
