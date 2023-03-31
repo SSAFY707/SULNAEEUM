6b23fdd719c90313b0fb2fde04e0da8948a8980a
김설희 <2750seolhee@naver.com>
2023-03-31 14:07:21 +0900

    [FEAT] RabbitMQ Consumer 구현

--- BE/sulnaeeum/src/main/java/com/ssafy/sulnaeeum/model/rabbitmq/service/Producer.java
+++ BE/sulnaeeum/src/main/java/com/ssafy/sulnaeeum/model/rabbitmq/service/Producer.java
@@ -9,10 +9,11 @@ import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
-public class ProducerService {
+public class Producer {
 
     private final RabbitTemplate rabbitTemplate;
 
+    // 메시지 전송
     public void sendMessage(Map<String, List> params) {
         rabbitTemplate.convertAndSend("sulnaeeum.exchange", "sulnaeeum.key", params);
     }
