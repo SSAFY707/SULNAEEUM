bb98e53563f37ecb3557ebb097fe4bf8c2566793
김원규 <re9946@naver.com>
2023-04-05 12:53:22 +0900

    [UPDATE] rabbitMQ test code 추가

---
+++ BE/data/Flask/cons2.py
@@ -0,0 +1,33 @@
+import pika
+import requests
+
+HOST_NAME = "j8a707.p.ssafy.io"
+QUEUE_NAME = "sulnaeeum.queue"
+
+
+def main():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=HOST_NAME))
+    channel = connection.channel()
+
+    channel.queue_declare(queue=QUEUE_NAME)
+
+    def callback(ch, method, properties, body):
+        print("Message is Arrived %r" % body)
+        response = requests.get("https://j8a707.p.ssafy.io/flask/test")
+        print(response)
+
+
+
+    channel.basic_consume(queue=QUEUE_NAME,
+                          on_message_callback=callback,
+                          auto_ack=True)
+
+    try:
+        print("Waiting for messages.")
+        channel.start_consuming()
+    except KeyboardInterrupt:
+        print('Ctrl+C is Pressed.')
+
+
+if __name__ == '__main__':
+    main()
